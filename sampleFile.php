d9c18111f9c59541c59011c9883f9ccfb3913924
swapnil_leadcode <swapnil.srivastava@leadcode.com>
2019-01-17 11:00:44 +0530

    testing

--- sampleFile.php
+++ sampleFile.php
@@ -23,12 +23,12 @@
  */
 
 // Configurations
-$access_token = 'EAAEIw2VSmIkBAEZBpFLoZBGE2GhW8JJMhGQaF8O3KlbpNnBJ3stLxvtZCFVNgZBXfm1qBktGn5p7nXbskjMtOAOmPzERglO0YhHp5hunL8BCfjwqEsmdWM1WSO8NDuPa7Ve9h7afTYvkUxLy9qtgX4CXUQqsnzCMFYbMoxUB5gnVuwNsQJBAPhpnI57EDQvTZACyXmQaalTQ3AWlB7aSlxYgGkFxpJ2oZD';
+$access_token = 'EAAEIw2VSmIkBAAuLXbXO5UhsHlWZBT6Je6nFKDY8Vz3wZB6JlpIpK9qL67RwEmcgy5YCnIZBvajXCKNAIeeC4ZB4hmZBlSXDfu7TX1Va9ZC5ZBRgWH03T2M77y19ghzjDrJqGgbwbO1Hm1wqcATOnbVnVl0ziUTuFarZBCmIdVWe42qNgqB62lPt';
 $app_id = '291110288267401';
 $app_secret = '69fc77650e02a05f1d260a460ea34b1f';
 // should begin with "act_" (eg: $account_id = 'act_1234567890';)
-$account_id = '309855264';
-define('SDK_DIR', __DIR__ . '/..'); // Path to the SDK directory
+$account_id = 'act_1166285790162852';
+define('SDK_DIR', __DIR__ . '/facebook-php-ads-sdk'); // Path to the SDK directory
 $loader = include SDK_DIR.'/vendor/autoload.php';
 date_default_timezone_set('America/Los_Angeles');
 // Configurations - End
@@ -76,12 +76,12 @@ if($account->{AdAccountFields::ACCOUNT_STATUS} !== 1) {
  */
 use FacebookAds\Object\Campaign;
 use FacebookAds\Object\Fields\CampaignFields;
-use FacebookAds\Object\Values\AdObjectives;
+use FacebookAds\Object\Values\AdAccountTargetingUnifiedObjectiveValues;
 
 $campaign  = new Campaign(null, $account->id);
 $campaign->setData(array(
 	CampaignFields::NAME => 'My First Campaign',
-	CampaignFields::OBJECTIVE => AdObjectives::LINK_CLICKS,
+	CampaignFields::OBJECTIVE => AdAccountTargetingUnifiedObjectiveValues::LINK_CLICKS,
 ));
 
 $campaign->validate()->create(array(
@@ -89,62 +89,55 @@ $campaign->validate()->create(array(
 ));
 echo "Campaign ID:" . $campaign->id . "\n";
 
-/**
- * Step 3 Search Targeting
- */
-use FacebookAds\Object\TargetingSearch;
-use FacebookAds\Object\Search\TargetingSearchTypes;
-use FacebookAds\Object\TargetingSpecs;
-use FacebookAds\Object\Fields\TargetingSpecsFields;
-
-$results = TargetingSearch::search(
-	$type = TargetingSearchTypes::INTEREST,
-	$class = null,
-	$query = 'facebook');
-
-// we'll take the top result for now
-$target = (count($results)) ? $results->current() : null;
-
-echo "Using target: ".$target->name."\n";
-
-$targeting = new TargetingSpecs();
-$targeting->{TargetingSpecsFields::GEO_LOCATIONS}
-           = array('countries' => array('GB'));
-$targeting->{TargetingSpecsFields::INTERESTS} = array(
-	array(
-		'id' => $target->id,
-		'name' => $target->name,
-	),
-);
 
 /**
  * Step 4 Create the AdSet
  */
 use FacebookAds\Object\AdSet;
-use FacebookAds\Object\Fields\AdSetFields;
-use FacebookAds\Object\Values\OptimizationGoals;
-use FacebookAds\Object\Values\BillingEvents;
+//use FacebookAds\Object\AdAccount;
 
-$adset = new AdSet(null, $account->id);
-$adset->setData(array(
-	AdSetFields::NAME => 'My First AdSet',
-	AdSetFields::CAMPAIGN_ID => $campaign->id,
-	AdSetFields::DAILY_BUDGET => '150',
-	AdSetFields::TARGETING => $targeting,
-	AdSetFields::OPTIMIZATION_GOAL => OptimizationGoals::REACH,
-	AdSetFields::BILLING_EVENT => BillingEvents::IMPRESSIONS,
-	AdSetFields::BID_AMOUNT => 100,
-	AdSetFields::START_TIME =>
-		(new \DateTime("+1 week"))->format(\DateTime::ISO8601),
-	AdSetFields::END_TIME =>
-		(new \DateTime("+2 week"))->format(\DateTime::ISO8601),
-));
+$fields = array(
+);
+$params = array(
+	'name' => 'A CPA Ad Set',
+	'campaign_id' => $campaign->id,
+	'daily_budget' => '5000',
+	'start_time' => (new \DateTime("+1 week"))->format(\DateTime::ISO8601),
+	'end_time' => (new \DateTime("+2 week"))->format(\DateTime::ISO8601),
+	'billing_event' => 'IMPRESSIONS',
+	'optimization_goal' => 'REACH',
+	'bid_amount' => '1000',
+//	'promoted_object' => array('page_id' => '<pageID>'),
+	'targeting' => array('geo_locations' => array('countries' => array('US'))),
+	'user_os' => 'iOS',
+	'publisher_platforms' => 'facebook',
+	'device_platforms' => 'mobile',
+);
+echo json_encode((new AdAccount($account->id))->createAdSet(
+	$fields,
+	$params
+)->exportAllData(), JSON_PRETTY_PRINT);
 
-$adset->validate()->create(array(
-	AdSet::STATUS_PARAM_NAME => AdSet::STATUS_ACTIVE,
-));
+//$adset = new AdSet(null, $account->id);
+//$adset->setData(array(
+//	AdSetFields::NAME => 'My First AdSet',
+//	AdSetFields::CAMPAIGN_ID => $campaign->id,
+//	AdSetFields::DAILY_BUDGET => '150',
+//	AdSetFields::TARGETING => $targeting,
+//	AdSetFields::OPTIMIZATION_GOAL => OptimizationGoals::REACH,
+//	AdSetFields::BILLING_EVENT => BillingEvents::IMPRESSIONS,
+//	AdSetFields::BID_AMOUNT => 100,
+//	AdSetFields::START_TIME =>
+//		(new \DateTime("+1 week"))->format(\DateTime::ISO8601),
+//	AdSetFields::END_TIME =>
+//		(new \DateTime("+2 week"))->format(\DateTime::ISO8601),
+//));
+//
+//$adset->validate()->create(array(
+//	AdSet::STATUS_PARAM_NAME => AdSet::STATUS_ACTIVE,
+//));
 
-echo 'AdSet  ID: '. $adset->id . "\n";
+//echo 'AdSet  ID: '. $adset->id . "\n";
 
 /**
  * Step 5 Create an AdImage
@@ -154,7 +147,7 @@ use FacebookAds\Object\Fields\AdImageFields;
 
 $image = new AdImage(null, $account->id);
 $image->{AdImageFields::FILENAME}
-       = SDK_DIR.'/test/misc/image.png';
+       = SDK_DIR.'/var/www/html/FBCampaign/facebook-php-ads-sdk/test/misc/image.png';
 
 $image->create();
 echo 'Image Hash: '.$image->hash . "\n";
